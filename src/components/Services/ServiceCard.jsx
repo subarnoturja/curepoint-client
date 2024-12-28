1a926e8f8f491781978e16f1fa0ab4a7d78d367b
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-28 14:41:26 +0600

    services section added

---
+++ src/components/Services/ServiceCard.jsx
@@ -0,0 +1,37 @@
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const ServiceCard = ({ item, index }) => {
+  const { name, desc, bgColor, textColor } = item;
+
+  return (
+    <div className="py-[30px] px-3 md:px-3">
+      <h2 className="text-[26px] leading-9 font-[600] text-headingColor">
+        {name}
+      </h2>
+      <p className="text-[16px] leading-7 font-[200] text-textColor mt-4">
+        {desc}
+      </p>
+      <div className="flex items-center justify-between mt-[30px]">
+        <Link
+          to="/doctors"
+          className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+        >
+          <BsArrowRight className="group-hover:text-white w-6 h-5"></BsArrowRight>
+        </Link>
+        <span
+          className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]"
+          style={{
+            background: `${bgColor}`,
+            color: `${textColor}`,
+            borderRadius: "6px 0 0 6px",
+          }}
+        >
+          {index + 1}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
