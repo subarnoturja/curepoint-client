9a7373f337371bf48c9758c59938310858aac6e8
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-01-16 12:54:09 +0600

    services page added

--- src/components/Services/ServiceList.jsx
+++ src/components/Services/ServiceList.jsx
@@ -3,7 +3,7 @@ import ServiceCard from "./ServiceCard";
 
 const ServiceList = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-[30px] mt-[30px] md:mt-[55px]">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-[30px]">
       {services.map((item, index) => (
         <ServiceCard key={index} item={item} index={index}></ServiceCard>
       ))}
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,18 @@
+import { services } from "../assets/data/services";
+import ServiceCard from "../components/Services/ServiceCard";
 
 const Services = () => {
-    return (
-        <div>
-           <h2>Services</h2> 
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-[30px] mt-[30px] md:mt-[55px]">
+          {services.map((item, index) => (
+            <ServiceCard key={index} item={item} index={index} />
+          ))}
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Services;
