5b0992ee7c46597c31a3190895ccb4b4db450b3f
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-01-18 20:38:05 +0600

    login page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+      email: '',
+      password: ''
+    })
+
+    const handleInputChange = e => {
+      setFormData({ ...formData, [e.target.name]:e.target.value })
+    }
+
     return (
-        <div className="font-[sans-serif]">
-      <div className="min-h-screen flex fle-col items-center justify-center py-6 px-4">
-        <div className="grid md:grid-cols-2 items-center gap-6 max-w-6xl w-full">
-          <div className="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
-            <form className="space-y-4">
-              <div className="mb-8">
-                <h3 className="text-gray-800 text-3xl font-bold">Sign in</h3>
-                <p className="text-gray-500 text-sm mt-4 leading-relaxed">Sign in to your account and explore a world of possibilities. Your journey begins here.</p>
+        <section className="px-4 md:px-0">
+          <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+            <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+            <span className="text-primaryColor">Login</span> Here
+            </h3>
+            {/* Form */}
+            <form className="py-4 md:py-0">
+              <div className="mb-5">
+                <input type="email" placeholder="Enter Your Email" name="email" value={formData.email} onChange={handleInputChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md" required />
               </div>
-
-              <div>
-                <label className="text-gray-800 text-sm mb-2 block">User name</label>
-                <div className="relative flex items-center">
-                  <input name="username" type="text" required className="w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600" placeholder="Enter user name" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-4" viewBox="0 0 24 24">
-                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
-                    <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
-                  </svg>
-                </div>
+              <div className="mb-5">
+                <input type="password" placeholder="Enter Your Password" name="password" value={formData.password} onChange={handleInputChange} className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md" required />
               </div>
-              <div>
-                <label className="text-gray-800 text-sm mb-2 block">Password</label>
-                <div className="relative flex items-center">
-                  <input name="password" type="password" required className="w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600" placeholder="Enter password" />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-4 cursor-pointer" viewBox="0 0 128 128">
-                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
-                  </svg>
-                </div>
+              <div className="mt-7">
+                <button type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3">Login</button>
               </div>
-
-              <div className="!mt-8">
-                <button type="button" className="w-full shadow-xl py-2.5 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
-                  Sign in
-                </button>
-              </div>
-
-              <p className="text-sm !mt-8 text-center text-gray-500">Don't have an account <Link to='/register' className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">Register here</Link></p>
+              <p className="mt-5 text-textColor text-center">Don&apos;t have an account?{""} <Link to='/register' className="text-primaryColor font-medium ml-1">Register</Link></p>
             </form>
           </div>
-          <div className="max-md:mt-8">
-            <img src="https://readymadeui.com/login-image.webp" className="w-full aspect-[71/50] max-md:w-4/5 mx-auto block object-cover" alt="Dining Experience" />
-          </div>
-        </div>
-      </div>
-    </div>
+        </section>
     );
 };
 
