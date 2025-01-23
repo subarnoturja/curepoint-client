b7f1f6031fc9ae7472b676d02e358f816dacad08
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-01-23 20:30:57 +0600

    some changes made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/curepoint.png";
 import userImg from "../../assets/images/avatar-icon.png";
 import { BiMenu } from "react-icons/bi";
 import { useEffect, useState } from "react";
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,6 +5,7 @@ const Services = () => {
   return (
     <section>
       <div className="container">
+        <h2 className="text-5xl text-center font-semibold">Services We Provide</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-[30px] mt-[30px] md:mt-[55px]">
           {services.map((item, index) => (
             <ServiceCard key={index} item={item} index={index} />
