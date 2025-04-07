6e3dd7896ff57c6f5900126c31dfed7633e35783
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-04-07 21:47:01 +0600

    photo url using cloudinary added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import signupImg from "../assets/images/signup.gif";
-import avatar from "../assets/images/doctor-img01.png";
 import { useState } from "react";
+import uploadImageToCloudinary from "../utils/uploadCloudinary";
+import { BASE_URL } from "../config";
+import { Bounce, toast } from "react-toastify";
+import { HashLoader } from "react-spinners";
 
 const Register = () => {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewURL, setPreviewURL] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const [formData, setFormData] = useState({
     name:'',  
@@ -16,6 +20,8 @@ const Register = () => {
     gender: '',
     role: 'patient',
     });
+
+    const navigate = useNavigate()
   
     const handleInputChange = (e) => {
       setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -23,11 +29,57 @@ const Register = () => {
 
     const handleFileInputChange = async(event) => {
       const file = event.target.files[0]
-      
+      const data = await uploadImageToCloudinary(file);
+      setPreviewURL(data.url)
+      setSelectedFile(data.url)
+      setFormData({...formData, photo:data.url})
     }
 
     const submitHandler = async event => {
       event.preventDefault();
+      setLoading(true)
+      try {
+        const res = await fetch(`${BASE_URL}/auth/register`, {
+          method: 'post',
+          headers: {
+            "Content-Type":'application/json'
+          },
+          body: JSON.stringify(formData)
+        })
+        const { message } = await res.json()
+
+        if(!res.ok){
+          throw new Error(message)
+        }
+
+        setLoading(false);
+        toast.success('Registration Successful', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+        navigate('/login')
+
+      } catch (error) {
+        toast.error(error.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+        setLoading(false)
+      }
     }
 
   return (
@@ -108,9 +160,9 @@ const Register = () => {
                 </label>
               </div>
               <div className="mb-5 flex items-center gap-3">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img src={avatar} alt="" className="w-full rounded-full" />
-                </figure>
+                { selectedFile && <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                  <img src={previewURL} alt="" className="w-full rounded-full" />
+                </figure>}
                 <div className="relative w-[160px] h-[50px]">
                   <input
                     type="file"
@@ -130,10 +182,11 @@ const Register = () => {
               </div>
               <div className="mt-7">
                 <button
+                  disabled={loading && true}
                   type="submit"
-                  className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
+                  className="btn w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
                 >
-                  Register
+                  { loading ? <HashLoader size={35} color="#ffffff" /> : "Register"}
                 </button>
               </div>
               <p className="mt-5 text-textColor text-center">
