ec683ff334b8a42957d1e7085daf48b49ed86a40
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-04-20 12:50:25 +0600

    logout functionality added

--- src/Dashboard/user-account/MyAccount.jsx
+++ src/Dashboard/user-account/MyAccount.jsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
 import userImg from '../../assets/images/doctor-img01.png'
+import { authContext } from '../../context/AuthContext';
 
 const MyAccount = () => {
+
+    const { dispatch } = useContext(authContext);
+
+    const handleLogout = () => {
+       dispatch({ type: 'LOGOUT' }); 
+    }
+
     return (
         <div className="max-w-[1170px] px-5 mx-auto">
            <div className="grid md:grid-cols-3 gap-10">
@@ -16,9 +25,12 @@ const MyAccount = () => {
                         <p className='text-textColor text-[14px] leading-6 font-medium'>Blood Type: <span className='ml-2 text-headingColor text-[16px] leading-8'>O+</span></p>
                     </div>
                     <div className='mt-[50px] md:mt-[100px]'>
-                        <button className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white text-lg'>Logout</button>
+                        <button onClick={handleLogout} className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white text-lg'>Logout</button>
                         <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white text-lg'>Delete Account</button>
                     </div>
+                </div>
+                <div>
+                    
                 </div>
            </div>
         </div>
