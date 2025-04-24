cf342270a2f25126861f914c2d82d927d0a9d259
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-04-24 20:43:39 +0600

    my booking page created

--- src/Dashboard/user-account/MyBookings.jsx
+++ src/Dashboard/user-account/MyBookings.jsx
@@ -1,11 +1,31 @@
-
+import DoctorCard from "../../components/Doctors/DoctorCard";
+import Error from "../../components/Error/Error";
+import Loading from "../../components/Loader/Loading";
+import { BASE_URL } from "../../config";
+import useFetchData from "../../hooks/useFetchData";
 
 const MyBookings = () => {
-    return (
-        <div>
-           <h2>My Bookings</h2> 
+  const {
+    data: appointments,
+    loading,
+    error,
+  } = useFetchData(`${BASE_URL}/users/appointments/my-appointments`);
+
+  return (
+    <div>
+      {loading && !error && <Loading />}
+      {error && !loading && <Error errorMessage={error} />}
+      {!loading && !error && (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {appointments.map((doctor) => (
+            <DoctorCard doctor={doctor} key={doctor._id} />
+          ))}
         </div>
-    );
+      )}
+      {!loading && !error && appointments.length === 0 && 
+      <h2 className="mt-5 text-center leading-7 text-[20px] font-semibold text-primaryColor">You did not book any doctor yet!</h2>}
+    </div>
+  );
 };
 
 export default MyBookings;
